144edfaa11ba0c4abb6acb4fb05389ac61f9fbdb
Sergey <sergo2591@gmail.com>
2022-06-18 12:50:28 +0400

    First commit

---
+++ resources/views/admin-panel.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+
+        Admin Panel
+
+    </div>
+
+</x-app-layout>
